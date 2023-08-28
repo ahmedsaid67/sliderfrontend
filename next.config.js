d8d4f5af6520a720a25a5ee8f3993f856b69c7b4
ahmedsaid67 <ahmetsaiddemirtas@gmail.com>
2023-08-29 00:13:01 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['127.0.0.1'], // Eğer sunucunuzun IP adresi yerine bir alan adı varsa, buraya ekleyin
+  },
+
 }
 
 module.exports = nextConfig
